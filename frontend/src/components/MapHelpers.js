62b41a9ddc765919ae89d9f2a1772834c4d261eb
Lilla Németh <naemly@protonmail.com>
2023-12-30 15:41:45 +0200

    moving functionalities from MapboxMap to MapHelpers

---
+++ frontend/src/components/MapHelpers.js
@@ -0,0 +1,104 @@
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
+
+export function setCoords(setter, value) {
+    setter(value);
+}
+
+export function changeCoordsByUser(map, setLng, setLat, setZoom) {
+    map.current.on('move', () => {
+        setLng(map.current.getCenter().lng.toFixed(4));
+        setLat(map.current.getCenter().lat.toFixed(4));
+        setZoom(map.current.getZoom().toFixed(2));
+    });
+}
+
+export function addNavigationButtons(map) {
+    const nav = new mapboxgl.NavigationControl();
+    map.current.addControl(nav, 'bottom-right');
+}
+
+export function addMaker(createProfile, map, lng, lat) {
+    new mapboxgl.Marker({ 
+        color: 'rgb(34, 102, 96)',
+        scale: 1.5,
+        draggable: createProfile ? true : false,
+    })
+    .setLngLat([lng, lat])
+    .addTo(map.current);
+}
+
+export function addFullscreenControl(map) {
+    const fullscreen = new mapboxgl.FullscreenControl();
+    map.current.addControl(fullscreen);
+}
+
+export function addGeolocateControl(map) {
+    const geolocate = new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true,
+        showUserHeading: true
+    });
+    map.current.addControl(geolocate);
+}
+
+export function addGeocoder(map) {
+    function coordinatesGeocoder(query) {
+        // Regex for lng and lat coords
+        const matches = query.match(
+            /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
+        );
+
+        if (!matches) {
+            return null;
+        }
+
+        // Reverse geocoding, converting geographic coordinates into a text description
+        function coordinateFeature(lng, lat) {
+            return {
+                center: [lng, lat],
+                geometry: {
+                    type: 'Point',
+                    coordinates: [lng, lat]
+                },
+                place_name: 'Lat: ' + lat + ' Lng: ' + lng,
+                place_type: ['coordinate'],
+                properties: {},
+                type: 'Feature'
+            }
+        }
+
+        const coord1 = Number(matches[1]);
+        const coord2 = Number(matches[2]);
+        const geocodes = [];
+
+        if (coord2 < -90 || coord2 > 90) {
+            geocodes.push(coordinateFeature(coord2, coord1));
+        }
+             
+        if (geocodes.length === 0) {
+            geocodes.push(coordinateFeature(coord1, coord2));
+            geocodes.push(coordinateFeature(coord2, coord1));
+        }
+             
+        return geocodes;
+    }
+
+    // Search location
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        localGeocoder: coordinatesGeocoder,
+        mapboxgl: mapboxgl,
+        marker: {
+            color: 'rgb(34, 102, 96)',
+            scale: 1.5
+        },
+        placeholder: 'Search location or coordinates',
+        reverseGeocode: true
+    });
+
+    map.current.addControl(geocoder);
+}
--- frontend/src/components/report_pet/MapboxMap.js
+++ frontend/src/components/report_pet/MapboxMap.js
@@ -3,44 +3,50 @@ import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-load
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import TextInput from '../generic/TextInput';
 import { ReactComponent as SearchIcon } from '../../assets/icons/search.svg';
+import { 
+    changeCoordsByUser, 
+    addNavigationButtons, 
+    addMaker, 
+    addFullscreenControl,
+    addGeolocateControl,
+    // addGeocoder,
+    setCoords
+} from '../MapHelpers';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
 
-const styles = {
-    categoryHeadline: {
-        padding: '45px 0px 15px',
-    }
-}
 
 const MapboxMap = (props) => {
     const { setLocation } = props;
-
+    
     // mapContainer renders the map inside a specific DOM element
+    // The ref will prevent the map from 
+    // reloading when the user interacts with the map
     const mapContainer = useRef(null);
-    // The ref will prevent the map from reloading when the user interacts with the map
     const map = useRef(null);
     const [lng, setLng] = useState(null);
     const [lat, setLat] = useState(null);
     const [zoom, setZoom] = useState(9);
     const [places, setPlaces] = useState([]);
-    // const [selectedPlace, setSelectedPlace] = useState([]);
     const [query, setQuery] = useState('');
     const [display, setDisplay] = useState('block');
+    // const [selectedPlace, setSelectedPlace] = useState([]);
+    
+    const mapStyle = 'mapbox://styles/l1ll4n3m/clqkvquob00mw01o939rncxn5';
 
     let DEBUG = false;
 
-    function getLocation(lng, lat) {
+    function getLocation() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
-                // Success:
-                showPosition, 
-                // Error:
-                fallbackPosition, 
+                showPosition,  // Success
+                fallbackPosition, // Error
                 {
                    enableHighAccuracy: true,
                    timeout: 5000,
                    maximumAge: 0
-                });
+                }
+            );
         } else { 
             console.log('Geolocation is not supported by your browser.');
         }
@@ -48,111 +54,18 @@ const MapboxMap = (props) => {
     
     function showPosition(position) {
         createMap(position.coords.longitude, position.coords.latitude);
+        setCoords(setLng, position.coords.longitude);
+        setCoords(setLat, position.coords.latitude);
     }
 
     function fallbackPosition() {
         // Helsinki fallback coords
-        createMap(24.9344, 60.1797);
-    }
+        const fallbackLng = 24.9344;
+        const fallbackLat = 60.1797;
 
-    function changeCoordsByUser(map) {
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
-
-            // setLocation([[map.current.getCenter().lng.toFixed(4)], [map.current.getCenter().lat.toFixed(4)]]);
-        });
-    }
-
-    function addNavigationButtons(map) {
-        const nav = new mapboxgl.NavigationControl();
-        map.current.addControl(nav, 'bottom-right');
-    }
-
-    function addMaker(map, lng, lat) {
-        new mapboxgl.Marker({ 
-            color: 'rgb(34, 102, 96)'
-        })
-        .setLngLat([lng, lat])
-        .addTo(map.current);
-    }
-
-    function addFullscreenControl(map) {
-        const fullscreen = new mapboxgl.FullscreenControl();
-        map.current.addControl(fullscreen);
-    }
-
-    function addGeolocateControl(map) {
-        const geolocate = new mapboxgl.GeolocateControl({
-            positionOptions: {
-                enableHighAccuracy: true
-            },
-            trackUserLocation: true,
-            showUserHeading: true
-        });
-        map.current.addControl(geolocate);
-    }
-
-    function addGeocoder(map) {
-        // function coordinatesGeocoder(query) {
-        //     // fetchSearchData(query);
-
-        //     // Regex for lng and lat coords
-        //     const matches = query.match(
-        //         /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
-        //     );
-
-        //     if (!matches) {
-        //         return null;
-        //     }
-
-        //     // Reverse geocoding, converting geographic coordinates into a text description
-        //     function coordinateFeature(lng, lat) {
-        //         return {
-        //             center: [lng, lat],
-        //             geometry: {
-        //                 type: 'Point',
-        //                 coordinates: [lng, lat]
-        //             },
-        //             place_name: 'Lat: ' + lat + ' Lng: ' + lng,
-        //             place_type: ['coordinate'],
-        //             properties: {},
-        //             type: 'Feature'
-        //         }
-        //     }
-
-        //     const coord1 = Number(matches[1]);
-        //     const coord2 = Number(matches[2]);
-        //     const geocodes = [];
-
-        //     if (coord2 < -90 || coord2 > 90) {
-        //         geocodes.push(coordinateFeature(coord2, coord1));
-        //     }
-                 
-        //     if (geocodes.length === 0) {
-        //         geocodes.push(coordinateFeature(coord1, coord2));
-        //         geocodes.push(coordinateFeature(coord2, coord1));
-        //     }
-                 
-        //     return geocodes;
-        // }
-
-        // Search location
-        // const geocoder = new MapboxGeocoder({
-        //     accessToken: mapboxgl.accessToken,
-        //     localGeocoder: coordinatesGeocoder,
-        //     mapboxgl: mapboxgl,
-        //     marker: {
-        //         color: 'rgb(34, 102, 96)',
-        //         scale: 1.5
-        //     },
-        //     placeholder: 'Search location or coordinates',
-        //     reverseGeocode: true
-        // });
-
-        // map.current.addControl(geocoder);
-        
+        createMap(fallbackLng, fallbackLat);
+        setCoords(setLng, fallbackLng);
+        setCoords(setLat, fallbackLat);
     }
 
     function createMap(lng, lat) {
@@ -160,36 +73,17 @@ const MapboxMap = (props) => {
         map.current = new mapboxgl.Map({
             accessToken: mapboxgl.accessToken,
             container: mapContainer.current,
-            style: 'mapbox://styles/l1ll4n3m/clqkvquob00mw01o939rncxn5',
+            style: mapStyle,
             center: [lng, lat],
             zoom: zoom
         });
 
-        changeCoordsByUser(map);
+        changeCoordsByUser(map, setLng, setLat, setZoom);
         addNavigationButtons(map);
-        addGeocoder(map);
-        addMaker(map, lng, lat);
+        addMaker(true, map, lng, lat);
         addFullscreenControl(map);
         addGeolocateControl(map);
-
-        // const geocoder = new MapboxGeocoder({
-        //     accessToken: mapboxgl.accessToken,
-        //     types: 'poi',
-        //     // see https://docs.mapbox.com/api/search/#geocoding-response-object for information about the schema of each response feature
-        //     render: function (item) {
-        //     // extract the item's maki icon or use a default
-        //     const maki = item.properties.maki || 'marker';
-        //     return `<div class='geocoder-dropdown-item'>
-        //     <img class='geocoder-dropdown-icon' src='https://unpkg.com/@mapbox/maki@6.1.0/icons/${maki}-15.svg'>
-        //     <span class='geocoder-dropdown-text'>
-        //     ${item.text}
-        //     </span>
-        //     </div>`;
-        //     },
-        //     mapboxgl: mapboxgl
-        // });
-
-        // map.current.addControl(geocoder);
+        // addGeocoder(map);
 
         // Clean up on unmount
         return () => map.current.remove();
@@ -219,13 +113,11 @@ const MapboxMap = (props) => {
 
     useEffect(() => {
         getLocation();
-        // createMap(lng, lat)
     }, []);
 
     useEffect(() => {
         fetchPlaces(query);
     }, [query]);
-
 
     return (
         <>
