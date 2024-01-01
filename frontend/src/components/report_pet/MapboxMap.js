07eb6722c3374f774859c5bde76476fe4ed198f4
Lilla Németh <naemly@protonmail.com>
2024-01-01 15:09:23 +0200

    Fixing dropdown clicking inside and outside of container; adding onFocus to search input

--- frontend/src/components/report_pet/MapboxMap.js
+++ frontend/src/components/report_pet/MapboxMap.js
@@ -27,12 +27,14 @@ const MapboxMap = (props) => {
     // reloading when the user interacts with the map
     const mapContainer = useRef(null);
     const map = useRef(null);
+    const dropdownRef = useRef(null);
+
     const [lng, setLng] = useState(null);
     const [lat, setLat] = useState(null);
     const [zoom, setZoom] = useState(9);
     const [places, setPlaces] = useState([]);
     const [query, setQuery] = useState('');
-    const [display, setDisplay] = useState('block');
+    const [display, setDisplay] = useState(false);
     // const [selectedPlace, setSelectedPlace] = useState([]);
     
     const mapStyle = 'mapbox://styles/l1ll4n3m/clqkvquob00mw01o939rncxn5';
@@ -56,9 +58,9 @@ const MapboxMap = (props) => {
     }
     
     function showPosition(position) {
-        createMap(position.coords.longitude, position.coords.latitude);
         setCoords(setLng, position.coords.longitude);
         setCoords(setLat, position.coords.latitude);
+        createMap(position.coords.longitude, position.coords.latitude);
     }
 
     function fallbackPosition() {
@@ -66,9 +68,9 @@ const MapboxMap = (props) => {
         const fallbackLng = 24.9344;
         const fallbackLat = 60.1797;
 
-        createMap(fallbackLng, fallbackLat);
         setCoords(setLng, fallbackLng);
         setCoords(setLat, fallbackLat);
+        createMap(fallbackLng, fallbackLat);
     }
 
     function createMap(lng, lat) {
@@ -97,14 +99,30 @@ const MapboxMap = (props) => {
 
         if (e.target.value) {
             fetchPlaces(query, setPlaces, lng, lat);
-            setDisplay('block');
         } else {
             setPlaces('');
         }
     }
 
+    const displayDropdown = () => {
+        setDisplay(!display);
+    };
+
+    function handleClickOutside(event) {
+        if (
+          dropdownRef.current &&
+          !dropdownRef.current.contains(event.target) &&
+          event.target !== dropdownRef.current
+        ) {
+          setDisplay(false);
+        }
+    };
+
     useEffect(() => {
         getLocation();
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
     }, []);
 
     useEffect(() => {
@@ -116,12 +134,18 @@ const MapboxMap = (props) => {
             <div className="sidebar" >
                 Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
             </div>
-            <div ref={mapContainer} className='map-container' />
+            <div 
+                ref={mapContainer} 
+                className='map-container' />
+            <div  
+                className='locationContainer' 
+                ref={dropdownRef} 
+                onClick={displayDropdown}>
                 <div className='inputBox' style={{padding: '10px 0 0 0 !important'}}>
                     <div className='searchButton'>
                         <SearchIcon onChange={handleChange} />
                     </div>
-                    <input 
+                    <input
                         className='formInput locationInput'
                         placeholder='search places' 
                         name='places' 
@@ -129,29 +153,32 @@ const MapboxMap = (props) => {
                         type='text'
                         value={query}
                         onChange={handleChange}
+                        onFocus={(e) => e.target.select()}
                     />
                 </div>
-            <div style={{display: display}}>
-                {places && places.map(place => {
-                    return (
-                        <div 
-                            className='locationSuggestion'
-                            key={place.id}
-                            onClick={() => {
-                                setQuery(place.place_name);
-                                setDisplay('none');
-                                // setSelectedPlace(place), 
-                                setLng(place.center[0]);
-                                setLat(place.center[1]);
-                                setLocation(`${place.center} ~~~ ${place.place_name}`);
-                                // console.log(lng, lat)
-                                // createMap(lng, lat)
-                            }}>
-                            <div className='locationSuggestionText'>{place.text}</div>
-                            <p className='locationSuggestionName'>{place.place_name}</p>   
-                        </div>
-                    )
-                })}
+                <div className='locationSuggestionContainer'>
+                    {display && places && places.map(place => {
+                        return (
+                            <div 
+                                  
+                                className='locationSuggestion'
+                                key={place.id}
+                                onClick={() => {
+                                    setQuery(place.place_name);
+                                    // setSelectedPlace(place), 
+                                    // setLng(place.center[0]);
+                                    // setLat(place.center[1]);
+                                    setCoords(setLng, place.center[0]);
+                                    setCoords(setLat, place.center[1]);
+                                    // createMap(lng, lat) 
+                                    setLocation(`${place.center} ~~~ ${place.place_name}`);
+                                }}>
+                                <div className='locationSuggestionText'>{place.text}</div>
+                                <p className='locationSuggestionName'>{place.place_name}</p>   
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </>
     );
