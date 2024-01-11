699badd29eab8f8c3413619d402ee83263f18ea9
Lilla Németh <naemly@protonmail.com>
2024-01-11 16:09:05 +0200

    moving X button outside of dropzone

--- frontend/src/components/report_pet/ImageUpload.js
+++ frontend/src/components/report_pet/ImageUpload.js
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 const ImageUpload = (props) => {
   const { 
     files,
+    file,
     setFiles,
     setFile, 
     getRootProps, 
@@ -13,8 +14,27 @@ const ImageUpload = (props) => {
 
   let DEBUG = false;
 
+  // function fileChangeHandler(event) {
+  //   // setFiles(event.target.files[0]);
+  //   setFile(URL.createObjectURL(event.target.files[0]));
+  //   console.log(event.target)
+  // }
+
   return (
     <>
+      {files && file ? (
+        <button
+          className='deleteButton'
+          // key={uuidv4()}
+          onClick={() => {
+            setFiles([]);
+            setFile('');
+          }}
+        >
+          <DeleteIcon />
+        </button>
+      ) : <></>
+      }
       <div {...getRootProps({
           className: 'imageUploadContainer'
         })}>
@@ -25,21 +45,12 @@ const ImageUpload = (props) => {
               name='file'
               id='file'
               className='inputFile'
+              // onChange={(e) => setFile(URL.createObjectURL(e.target.files[0]))}
               {...getInputProps()}
             />
-            Choose a file
+            Drop a file
             {files.map(file => (
               <>
-              <button
-                className='deleteButton'
-                key={uuidv4()}
-                onClick={() => {
-                  setFiles([]);
-                  setFile('');
-                }}
-              >
-                <DeleteIcon />
-              </button>
               <img 
                 key={file.name} 
                 src={file.preview} 
