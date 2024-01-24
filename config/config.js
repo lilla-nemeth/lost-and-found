910da48d10f00f77646ffcb8456b6b5083593af5
Lilla Németh <naemly@protonmail.com>
2024-01-24 20:54:22 +0200

    sequelize init folders, files; changing config.json to config.js with environment variables of db

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+	development: {
+		username: process.env.PG_USER,
+		password: process.env.PG_PASSWORD,
+		database: process.env.PG_DATABASE,
+		host: process.env.PG_HOST,
+		dialect: 'postgres',
+	},
+	test: {
+		username: 'root',
+		password: null,
+		database: 'database_production',
+		host: '127.0.0.1',
+		dialect: 'postgres',
+	},
+	production: {
+		username: 'root',
+		password: null,
+		database: 'database_production',
+		host: '127.0.0.1',
+		dialect: 'postgres',
+	},
+};
