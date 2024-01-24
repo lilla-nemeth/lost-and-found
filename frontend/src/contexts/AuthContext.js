cb416389f4cdac07ae1ff788434ffc82e83ee463
Lilla Németh <naemly@protonmail.com>
2024-01-24 18:33:09 +0200

    AuthContext prettier

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -3,28 +3,22 @@ import React, { createContext, useState, useEffect } from 'react';
 export const AuthContext = createContext();
 
 export default function AuthContextProvider(props) {
-    const [token, setToken] = useState(null);
+	const [token, setToken] = useState(null);
 
-    let DEBUG = false;
+	let DEBUG = false;
 
-    useEffect(() => {
+	useEffect(() => {
+		let tokenFromLocalStorage = localStorage.getItem('token');
 
-       let tokenFromLocalStorage = localStorage.getItem('token');
+		if (tokenFromLocalStorage) {
+			setToken(tokenFromLocalStorage);
+		}
+	}, [token]);
 
-       if (tokenFromLocalStorage) {
-           setToken(tokenFromLocalStorage);
-       }
+	function handleLogOut() {
+		localStorage.removeItem('token');
+		setToken(null);
+	}
 
-    },[token]);
-    
-    function handleLogOut() {
-        localStorage.removeItem('token');
-        setToken(null);
-    }
-
-    return (
-        <AuthContext.Provider value={{token, setToken, handleLogOut}}>
-            { props.children }
-        </AuthContext.Provider>
-    );
+	return <AuthContext.Provider value={{ token, setToken, handleLogOut }}>{props.children}</AuthContext.Provider>;
 }
