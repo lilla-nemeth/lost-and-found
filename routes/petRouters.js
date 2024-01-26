8dcf65db19816ee30b5c1f5382b0dc1caf37297b
Lilla Németh <naemly@protonmail.com>
2024-01-26 14:39:04 +0200

    routes folder for router files

---
+++ routes/petRouters.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const pets = () => {
+	const router = Router();
+};
+
+export default pets;
---
+++ routes/userRouters.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const users = () => {
+	const router = Router();
+};
+
+export default users;
