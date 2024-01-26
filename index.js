27d30ed49b7e9e1ebc79bda98000036ee1c7707b
Lilla Németh <naemly@protonmail.com>
2024-01-26 15:05:51 +0200

    cleanup index.js, queries have been moved to queries.js

--- index.js
+++ index.js
@@ -1,333 +1,44 @@
 import express from 'express';
 import cors from 'cors';
-import pg from 'pg';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import path from 'path';
-import axios from 'axios';
-import url from 'url';
 import dotenv from 'dotenv';
 import { authMw, isFormValid, upload } from './middlewares.js';
 import * as queries from './queries/queries.js';
 
-const app = express();
-const { Pool } = pg;
 dotenv.config();
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-import {
-	ERROR_MSG_FETCH_USER_PETS,
-	ERROR_MSG_FETCH_ALL_PETS,
-	ERROR_MSG_FETCH_PETS,
-	ERROR_MSG_FETCH_TOTAL_PETS,
-	ERROR_MSG_FETCH_PET,
-	ERROR_MSG_UPDATE_PET,
-	ERROR_MSG_FETCH_USERNAME,
-	ERROR_MSG_FETCH_USER,
-	ERROR_MSG_UPDATE_USER,
-	ERROR_MSG_DELETE_PET,
-	ERROR_MSG_DELETE_PETS,
-	ERROR_MSG_CREATE_PET,
-	ERROR_MSG_DELETE_USER,
-	ERROR_MSG_USED_EMAIL,
-	ERROR_MSG_USED_PHONE,
-	ERROR_MSG_INCORRECT_PASSWORD,
-	ERROR_MSG_NOT_FOUND_USER,
-	SUCCESS_MSG_CREATED_USER,
-	SUCCESS_MSG_UPDATED_PET,
-	SUCCESS_MSG_UPDATED_USER,
-	SUCCESS_MSG_DELETED_PET,
-	SUCCESS_MSG_DELETED_PETS,
-	SUCCESS_MSG_DELETED_USER_AND_PETS,
-} from './types/messageTypes.js';
-
-import {
-	SELECT_PETS_BY_DESC_DATE,
-	SELECT_PETS_BY_PAGINATION,
-	SELECT_TOTAL_NUM_OF_PETS,
-	SELECT_PET_BY_ID,
-	SELECT_ALL_USERS,
-	SELECT_USER_BY_ID,
-	SELECT_PETS_BY_USER,
-	SELECT_USER_BY_EMAIL,
-	UPDATE_PET,
-	UPDATE_USER,
-	DELETE_ALL_PETS,
-	DELETE_PET_BY_ID,
-	DELETE_PET_BY_USER,
-	DELETE_USER_BY_ID,
-	INSERT_PET_VALUES,
-	INSERT_USER_VALUES,
-} from './types/queryTypes.js';
 
 let DEBUG = false;
 
 const port = process.env.PORT || 3003;
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'frontend/build')));
-}
-
-const devSettings = {
-	host: process.env.PG_HOST,
-	user: process.env.PG_USER,
-	password: process.env.PG_PASSWORD,
-	port: process.env.PG_PORT,
-	database: process.env.PG_DATABASE,
-};
-
-const prodSettings = {
-	connectionString: process.env.DATABASE_URL,
-	ssl: {
-		rejectUnauthorized: process.env.NODE_ENV === 'production' ? false : true,
-	},
-};
-
-const pool = new Pool(process.env.NODE_ENV === 'production' ? prodSettings : devSettings);
-
+// GET
 app.get('/userpets', authMw, queries.getAllUserPets);
 app.get('/allpets', queries.getAllPets);
 app.get('/pets/:fetch/:skip', queries.getPetsByPagination);
+app.get('/pets/total', queries.getTotalNumberOfPets);
+app.get('/pets/:id', queries.getPetById);
+app.get('/username', authMw, queries.getUsername);
+app.get('/users', authMw, queries.getAllUsers);
+app.get('/locationsearch/:query', queries.getGeocodeLocation);
 
-// get the total amount (number) of pets
-app.get('/pets/total', (request, response) => {
-	pool
-		.query(SELECT_TOTAL_NUM_OF_PETS)
-		.then((res) => response.status(200).json(res.rows[0].count))
-		.catch((err) => response.status(400).json({ msg: ERROR_MSG_FETCH_TOTAL_PETS }));
-});
+// PUT
+app.put('/editpet/:id', authMw, queries.updatePetData);
+app.put('/editprofile', [authMw, isFormValid], queries.updateUserData);
 
-// from pets table get one pet by id
-app.get('/pets/:id', (request, response) => {
-	let id = request.params.id;
+// DELETE
+app.delete('/deletepet/:id', authMw, queries.deleteUserPet);
+app.delete('/deleteallpets', authMw, queries.deleteAllUserPets);
+app.delete('/deleteuser', authMw, queries.deleteUser);
 
-	pool
-		.query(SELECT_PET_BY_ID, [id])
-		.then((res) => response.status(200).json(res.rows))
-		.catch((err) => response.status(400).json({ msg: ERROR_MSG_FETCH_PET }));
-});
+// POST
+app.post('/register', [isFormValid], queries.createAccount);
+app.post('/login', [isFormValid], queries.signIn);
+app.post('/reportpet', [authMw, upload.single('file')], queries.createPetProfile);
 
-app.get('/username', authMw, (request, response) => {
-	let id = request.userId;
-
-	pool
-		.query(SELECT_USER_BY_ID, [id])
-		.then((res) => response.status(200).json(res.rows[0].username))
-		.catch((err) => response.status(400).json({ msg: ERROR_MSG_FETCH_USERNAME }));
-});
-
-// get all users
-app.get('/users', authMw, (request, response) => {
-	pool
-		.query(SELECT_ALL_USERS)
-		.then((res) => response.status(200).json(res.rows))
-		.catch((err) => response.status(400).json({ msg: ERROR_MSG_FETCH_USER }));
-});
-
-// search pet location
-app.get('/locationsearch/:query', (request, response) => {
-	const query = request.params.query;
-	const params = new URLSearchParams({
-		access_token: process.env.API_KEY,
-		...url.parse(request.url, true).query,
-	});
-	const result = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`;
-
-	axios
-		.get(result)
-		.then((res) => {
-			response.status(200).json(res.data);
-		})
-		.catch((err) => {
-			response.status(500).json({ error: err.message });
-		});
-});
-
-// edit pet by user (user dashboard)
-app.put('/editpet/:id', authMw, (request, response) => {
-	let id = request.params.id;
-	let petstatus = request.body.petstatus;
-	let petlocation = request.body.petlocation;
-	let longitude = request.body.longitude;
-	let latitude = request.body.latitude;
-	let species = request.body.species;
-	let petsize = request.body.petsize;
-	let breed = request.body.breed;
-	let sex = request.body.sex;
-	let color = request.body.color;
-	let age = request.body.age;
-	let uniquefeature = request.body.uniquefeature;
-	let postdescription = request.body.postdescription;
-
-	pool
-		.query(UPDATE_PET, [
-			petstatus,
-			petlocation,
-			longitude,
-			latitude,
-			species,
-			petsize,
-			breed,
-			sex,
-			color,
-			age,
-			uniquefeature,
-			postdescription,
-			id,
-		])
-		.then((res) => response.status(200).json({ msg: SUCCESS_MSG_UPDATED_PET }))
-		.catch((err) => response.status(400).json({ msg: ERROR_MSG_UPDATE_PET }));
-});
-
-// edit user data (user dashboard)
-app.put('/editprofile', [authMw, isFormValid], (request, response) => {
-	let id = request.userId;
-	let username = request.body.username;
-	let email = request.body.email;
-	let pw = request.body.pw;
-	let phone = request.body.phone;
-	let encryptedPw = bcrypt.hashSync(pw, 10);
-
-	pool
-		.query(UPDATE_USER, [username, email, encryptedPw, phone, id])
-		.then((res) => response.status(200).json({ msg: SUCCESS_MSG_UPDATED_USER }))
-		.catch((err) => response.status(400).json({ msg: ERROR_MSG_UPDATE_USER }));
-});
-
-// delete 1 pet by user (user dashboard)
-app.delete('/deletepet/:id', authMw, (request, response) => {
-	let id = request.params.id;
-	// let isadmin = request.isadmin;
-
-	pool
-		.query(DELETE_PET_BY_ID, [id])
-		.then((res) => response.status(200).json({ msg: SUCCESS_MSG_DELETED_PET }))
-		.catch((err) => response.status(400).json({ msg: ERROR_MSG_DELETE_PET }));
-});
-
-// delete all pets by user (user dashboard)
-app.delete('/deleteallpets', authMw, (request, response) => {
-	let userId = request.userId;
-	let isadmin = request.isadmin;
-
-	let adminQuery = DELETE_ALL_PETS;
-	let userQuery = DELETE_PET_BY_USER;
-
-	pool
-		.query(isadmin ? adminQuery : userQuery, [userId])
-		.then((res) => response.status(200).json({ msg: SUCCESS_MSG_DELETED_PETS }))
-		.catch((err) => response.status(400).json({ msg: ERROR_MSG_DELETE_PETS }));
-});
-
-// delete user - delete user and the connected pets (user dashboard)
-app.delete('/deleteuser', authMw, (request, response) => {
-	let userId = request.userId;
-
-	pool
-		.query(DELETE_PET_BY_USER, [userId])
-		.then((res) => {
-			pool
-				.query(DELETE_USER_BY_ID, [userId])
-				.then((res) =>
-					response.status(200).json({
-						msg: SUCCESS_MSG_DELETED_USER_AND_PETS,
-					})
-				)
-				.catch((err) => response.status(400).json({ msg: ERROR_MSG_DELETE_USER }));
-		})
-		.catch((err) => response.status(400).json({ msg: ERROR_MSG_DELETE_PETS }));
-});
-
-// register
-app.post('/register', [isFormValid], (request, response) => {
-	let username = request.body.username;
-	let email = request.body.email;
-	let pw = request.body.pw;
-	let phone = request.body.phone;
-	let encryptedPw = bcrypt.hashSync(pw, 10);
-
-	pool
-		.query(INSERT_USER_VALUES, [username, email, encryptedPw, phone])
-		.then((res) => response.status(200).json({ msg: SUCCESS_MSG_CREATED_USER }))
-		.catch((err) => {
-			if (err.code === '23505' && err.constraint === 'users_email_key') {
-				response.status(400).json({ msg: ERROR_MSG_USED_EMAIL });
-			} else if (err.code === '23505' && err.constraint === 'users_phone_key') {
-				response.status(400).json({ msg: ERROR_MSG_USED_PHONE });
-			} else if (err.code != '23505' && isFormValid) {
-				isFormValid;
-			}
-		});
-});
-
-// login
-app.post('/login', [isFormValid], (request, response) => {
-	let email = request.body.email;
-	let pw = request.body.pw;
-
-	pool
-		.query(SELECT_USER_BY_EMAIL, [email])
-		.then((res) => {
-			let userObject = res.rows[0];
-			let encryptedPw = userObject.pw;
-
-			res.rows &&
-				bcrypt.compare(pw, encryptedPw).then((isMatch) => {
-					if (isMatch) {
-						jwt.sign({ id: userObject.id, isadmin: userObject.isadmin }, 'r4uqSKqC6L', (err, token) => {
-							response.status(200).json(token);
-						});
-					} else {
-						response.status(403).json({ msg: ERROR_MSG_INCORRECT_PASSWORD });
-					}
-				});
-		})
-		.catch((err) => response.status(400).json({ msg: ERROR_MSG_NOT_FOUND_USER }));
-});
-
-// report pet by user
-app.post('/reportpet', [authMw, upload.single('file')], (request, response) => {
-	let userId = request.userId;
-	let img = request.file.buffer.toString('base64');
-	let petstatus = request.body.petstatus;
-	let petlocation = request.body.petlocation;
-	let longitude = request.body.longitude;
-	let latitude = request.body.latitude;
-	let species = request.body.species;
-	let petsize = request.body.petsize;
-	let breed = request.body.breed;
-	let sex = request.body.sex;
-	let color = request.body.color;
-	let age = request.body.age;
-	let uniquefeature = request.body.uniquefeature;
-	let postdescription = request.body.postdescription;
-
-	pool
-		.query(INSERT_PET_VALUES, [
-			userId,
-			img,
-			petstatus,
-			petlocation,
-			longitude,
-			latitude,
-			species,
-			petsize,
-			breed,
-			sex,
-			color,
-			age,
-			uniquefeature,
-			postdescription,
-		])
-		.then((res) => response.status(200).json(res.rows))
-		.catch((err) => response.status(400).json({ msg: ERROR_MSG_CREATE_PET }));
-});
-
-app.get('*', (request, response) => {
-	const __dirname = path.resolve(path.dirname(''));
-	response.sendFile(path.join(__dirname, 'frontend/build/index.html'));
-});
+app.get('*', queries.getAll);
 
 app.listen(port, () => console.log('Server is running on 3003'));
--- queries/queries.js
+++ queries/queries.js
@@ -1,11 +1,21 @@
 import pg from 'pg';
 import * as messages from '../types/messageTypes.js';
 import * as queries from '../types/queryTypes.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import path from 'path';
+import axios from 'axios';
+import url from 'url';
 import dotenv from 'dotenv';
+import { request } from 'express';
 
 dotenv.config();
 
 const { Pool } = pg;
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'frontend/build')));
+}
 
 const devSettings = {
 	host: process.env.PG_HOST,
@@ -26,11 +36,10 @@ const pool = new Pool(process.env.NODE_ENV === 'production' ? prodSettings : dev
 
 // get all pets by userId
 const getAllUserPets = (request, response) => {
-	let userId = request.userId;
-	let isadmin = request.isadmin;
-
-	let adminQuery = queries.SELECT_PETS_BY_DESC_DATE;
-	let userQuery = queries.SELECT_PETS_BY_USER;
+	const userId = request.userId;
+	const isadmin = request.isadmin;
+	const adminQuery = queries.SELECT_PETS_BY_DESC_DATE;
+	const userQuery = queries.SELECT_PETS_BY_USER;
 
 	pool
 		.query(isadmin ? adminQuery : userQuery, [userId])
@@ -48,8 +57,8 @@ const getAllPets = (request, response) => {
 
 // get/fetch limited amount of pets to pagination
 const getPetsByPagination = (request, response) => {
-	let limit = request.params.fetch;
-	let offset = request.params.skip;
+	const limit = request.params.fetch;
+	const offset = request.params.skip;
 
 	pool
 		.query(queries.SELECT_PETS_BY_PAGINATION, [limit, offset])
@@ -57,4 +66,259 @@ const getPetsByPagination = (request, response) => {
 		.catch((err) => response.status(400).json({ msg: messages.ERROR_MSG_FETCH_PETS }));
 };
 
-export { getAllUserPets, getAllPets, getPetsByPagination };
+// get the total amount (number) of pets
+const getTotalNumberOfPets = (request, response) => {
+	pool
+		.query(SELECT_TOTAL_NUM_OF_PETS)
+		.then((res) => response.status(200).json(res.rows[0].count))
+		.catch((err) => response.status(400).json({ msg: ERROR_MSG_FETCH_TOTAL_PETS }));
+};
+
+// from pets table get one pet by id
+const getPetById = (request, response) => {
+	const id = request.params.id;
+
+	pool
+		.query(SELECT_PET_BY_ID, [id])
+		.then((res) => response.status(200).json(res.rows))
+		.catch((err) => response.status(400).json({ msg: ERROR_MSG_FETCH_PET }));
+};
+
+// get username
+const getUsername = (request, response) => {
+	const id = request.userId;
+
+	pool
+		.query(SELECT_USER_BY_ID, [id])
+		.then((res) => response.status(200).json(res.rows[0].username))
+		.catch((err) => response.status(400).json({ msg: ERROR_MSG_FETCH_USERNAME }));
+};
+
+// get all users
+const getAllUsers = (request, response) => {
+	pool
+		.query(SELECT_ALL_USERS)
+		.then((res) => response.status(200).json(res.rows))
+		.catch((err) => response.status(400).json({ msg: ERROR_MSG_FETCH_USER }));
+};
+
+// search pet location
+const getGeocodeLocation = (request, response) => {
+	const query = request.params.query;
+	const params = new URLSearchParams({
+		access_token: process.env.API_KEY,
+		...url.parse(request.url, true).query,
+	});
+	const result = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`;
+
+	axios
+		.get(result)
+		.then((res) => {
+			response.status(200).json(res.data);
+		})
+		.catch((err) => {
+			response.status(500).json({ error: err.message });
+		});
+};
+
+// edit pet by user (user dashboard)
+const updatePetData = (request, response) => {
+	const id = request.params.id;
+	const petstatus = request.body.petstatus;
+	const petlocation = request.body.petlocation;
+	const longitude = request.body.longitude;
+	const latitude = request.body.latitude;
+	const species = request.body.species;
+	const petsize = request.body.petsize;
+	const breed = request.body.breed;
+	const sex = request.body.sex;
+	const color = request.body.color;
+	const age = request.body.age;
+	const uniquefeature = request.body.uniquefeature;
+	const postdescription = request.body.postdescription;
+
+	pool
+		.query(UPDATE_PET, [
+			petstatus,
+			petlocation,
+			longitude,
+			latitude,
+			species,
+			petsize,
+			breed,
+			sex,
+			color,
+			age,
+			uniquefeature,
+			postdescription,
+			id,
+		])
+		.then((res) => response.status(200).json({ msg: SUCCESS_MSG_UPDATED_PET }))
+		.catch((err) => response.status(400).json({ msg: ERROR_MSG_UPDATE_PET }));
+};
+
+// edit user data (user dashboard)
+const updateUserData = (request, response) => {
+	let id = request.userId;
+	let username = request.body.username;
+	let email = request.body.email;
+	let pw = request.body.pw;
+	let phone = request.body.phone;
+	let encryptedPw = bcrypt.hashSync(pw, 10);
+
+	pool
+		.query(UPDATE_USER, [username, email, encryptedPw, phone, id])
+		.then((res) => response.status(200).json({ msg: SUCCESS_MSG_UPDATED_USER }))
+		.catch((err) => response.status(400).json({ msg: ERROR_MSG_UPDATE_USER }));
+};
+
+// delete 1 pet by user (user dashboard)
+const deleteUserPet = (request, response) => {
+	let id = request.params.id;
+
+	pool
+		.query(DELETE_PET_BY_ID, [id])
+		.then((res) => response.status(200).json({ msg: SUCCESS_MSG_DELETED_PET }))
+		.catch((err) => response.status(400).json({ msg: ERROR_MSG_DELETE_PET }));
+};
+
+// delete all pets by user (user dashboard)
+const deleteAllUserPets = (request, response) => {
+	let userId = request.userId;
+	let isadmin = request.isadmin;
+
+	let adminQuery = DELETE_ALL_PETS;
+	let userQuery = DELETE_PET_BY_USER;
+
+	pool
+		.query(isadmin ? adminQuery : userQuery, [userId])
+		.then((res) => response.status(200).json({ msg: SUCCESS_MSG_DELETED_PETS }))
+		.catch((err) => response.status(400).json({ msg: ERROR_MSG_DELETE_PETS }));
+};
+
+// delete user - delete user and the connected pets (user dashboard)
+const deleteUser = (request, response) => {
+	let userId = request.userId;
+
+	pool
+		.query(DELETE_PET_BY_USER, [userId])
+		.then((res) => {
+			pool
+				.query(DELETE_USER_BY_ID, [userId])
+				.then((res) =>
+					response.status(200).json({
+						msg: SUCCESS_MSG_DELETED_USER_AND_PETS,
+					})
+				)
+				.catch((err) => response.status(400).json({ msg: ERROR_MSG_DELETE_USER }));
+		})
+		.catch((err) => response.status(400).json({ msg: ERROR_MSG_DELETE_PETS }));
+};
+
+const createAccount = (request, response) => {
+	let username = request.body.username;
+	let email = request.body.email;
+	let pw = request.body.pw;
+	let phone = request.body.phone;
+	let encryptedPw = bcrypt.hashSync(pw, 10);
+
+	pool
+		.query(INSERT_USER_VALUES, [username, email, encryptedPw, phone])
+		.then((res) => response.status(200).json({ msg: SUCCESS_MSG_CREATED_USER }))
+		.catch((err) => {
+			if (err.code === '23505' && err.constraint === 'users_email_key') {
+				response.status(400).json({ msg: ERROR_MSG_USED_EMAIL });
+			} else if (err.code === '23505' && err.constraint === 'users_phone_key') {
+				response.status(400).json({ msg: ERROR_MSG_USED_PHONE });
+			} else if (err.code != '23505' && isFormValid) {
+				isFormValid;
+			}
+		});
+};
+
+const signIn = (request, response) => {
+	let email = request.body.email;
+	let pw = request.body.pw;
+
+	pool
+		.query(SELECT_USER_BY_EMAIL, [email])
+		.then((res) => {
+			let userObject = res.rows[0];
+			let encryptedPw = userObject.pw;
+
+			res.rows &&
+				bcrypt.compare(pw, encryptedPw).then((isMatch) => {
+					if (isMatch) {
+						jwt.sign({ id: userObject.id, isadmin: userObject.isadmin }, 'r4uqSKqC6L', (err, token) => {
+							response.status(200).json(token);
+						});
+					} else {
+						response.status(403).json({ msg: ERROR_MSG_INCORRECT_PASSWORD });
+					}
+				});
+		})
+		.catch((err) => response.status(400).json({ msg: ERROR_MSG_NOT_FOUND_USER }));
+};
+
+// report pet by user
+const createPetProfile = (request, response) => {
+	const userId = request.userId;
+	const img = request.file.buffer.toString('base64');
+	const petstatus = request.body.petstatus;
+	const petlocation = request.body.petlocation;
+	const longitude = request.body.longitude;
+	const latitude = request.body.latitude;
+	const species = request.body.species;
+	const petsize = request.body.petsize;
+	const breed = request.body.breed;
+	const sex = request.body.sex;
+	const color = request.body.color;
+	const age = request.body.age;
+	const uniquefeature = request.body.uniquefeature;
+	const postdescription = request.body.postdescription;
+
+	pool
+		.query(INSERT_PET_VALUES, [
+			userId,
+			img,
+			petstatus,
+			petlocation,
+			longitude,
+			latitude,
+			species,
+			petsize,
+			breed,
+			sex,
+			color,
+			age,
+			uniquefeature,
+			postdescription,
+		])
+		.then((res) => response.status(200).json(res.rows))
+		.catch((err) => response.status(400).json({ msg: ERROR_MSG_CREATE_PET }));
+};
+
+const getAll = (request, response) => {
+	const __dirname = path.resolve(path.dirname(''));
+	response.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+};
+
+export {
+	getAllUserPets,
+	getAllPets,
+	getPetsByPagination,
+	getTotalNumberOfPets,
+	getPetById,
+	getUsername,
+	getAllUsers,
+	getGeocodeLocation,
+	updatePetData,
+	updateUserData,
+	deleteUserPet,
+	deleteAllUserPets,
+	deleteUser,
+	createAccount,
+	signIn,
+	createPetProfile,
+	getAll,
+};
