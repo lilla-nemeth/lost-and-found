c55f6a33f0fc33bdb6d908cff4c13535a71132ef
Lilla Németh <naemly@protonmail.com>
2024-01-26 14:35:18 +0200

    moving sql queries and error and success messages constants to types folder

---
+++ types/messageTypes.js
@@ -0,0 +1,75 @@
+/* Success Messages */
+const IS = `is`;
+const ARE = `are`;
+const SUCCESSFULLY = `succesfully`;
+const UPDATED = `updated`;
+const DELETED = `deleted`;
+const CREATED = `created`;
+
+/* Pet Success Messages */
+const SUCCESS_MSG_UPDATED_PET = `Your post ${IS} ${SUCCESSFULLY} ${UPDATED}`;
+const SUCCESS_MSG_DELETED_PET = `Your post ${IS} ${SUCCESSFULLY} ${DELETED}`;
+const SUCCESS_MSG_DELETED_PETS = `Your posts ${ARE} ${SUCCESSFULLY} ${DELETED}`;
+
+/* User Success Messages */
+const SUCCESS_MSG_CREATED_USER = `Your account ${IS} ${SUCCESSFULLY} ${CREATED}`;
+const SUCCESS_MSG_UPDATED_USER = `Your profile ${IS} ${SUCCESSFULLY} ${UPDATED}`;
+
+const SUCCESS_MSG_DELETED_USER_AND_PETS = `Your account and your posts ${ARE} ${SUCCESSFULLY} ${DELETED}`;
+
+/* Error Messages */
+const FAILED_TO = `Failed to`;
+const FETCH = `fetch`;
+const UPDATE = `update`;
+const DELETE = `delete`;
+const ADD = `add`;
+const ALREADY = `already`;
+const EXISTS = `exists`;
+const PLEASE_TRY_ANOTHER_ONE = `Please try another one.`;
+
+/* Pet Error Messages */
+const ERROR_MSG_FETCH_USER_PETS = `${FAILED_TO} ${FETCH} your pets.`;
+const ERROR_MSG_FETCH_ALL_PETS = `${FAILED_TO} ${FETCH} all pets.`;
+const ERROR_MSG_FETCH_PETS = `${FAILED_TO} ${FETCH} pets.`;
+const ERROR_MSG_FETCH_TOTAL_PETS = `${FAILED_TO} ${FETCH} the number of pets.`;
+const ERROR_MSG_FETCH_PET = `${FAILED_TO} ${FETCH} pet.`;
+const ERROR_MSG_UPDATE_PET = `${FAILED_TO} ${UPDATE} your post.`;
+const ERROR_MSG_DELETE_PET = `${FAILED_TO} ${DELETE} your post.`;
+const ERROR_MSG_DELETE_PETS = `${FAILED_TO} ${DELETE} your posts.`;
+const ERROR_MSG_CREATE_PET = `${FAILED_TO} ${ADD} new pet.`;
+
+/* User Error Messages */
+const ERROR_MSG_FETCH_USERNAME = `${FAILED_TO} ${FETCH} username.`;
+const ERROR_MSG_FETCH_USER = `${FAILED_TO} ${FETCH} user.`;
+const ERROR_MSG_UPDATE_USER = `${FAILED_TO} ${UPDATE} your profile.`;
+const ERROR_MSG_DELETE_USER = `${FAILED_TO} ${DELETE} your account.`;
+const ERROR_MSG_USED_EMAIL = `Email address ${ALREADY} ${EXISTS}. ${PLEASE_TRY_ANOTHER_ONE}`;
+const ERROR_MSG_USED_PHONE = `Phone number ${ALREADY} ${EXISTS}. ${PLEASE_TRY_ANOTHER_ONE}`;
+const ERROR_MSG_INCORRECT_PASSWORD = `Your password ${IS} incorrect`;
+const ERROR_MSG_NOT_FOUND_USER = `User not found.`;
+
+export {
+	ERROR_MSG_FETCH_USER_PETS,
+	ERROR_MSG_FETCH_ALL_PETS,
+	ERROR_MSG_FETCH_PETS,
+	ERROR_MSG_FETCH_TOTAL_PETS,
+	ERROR_MSG_FETCH_PET,
+	ERROR_MSG_UPDATE_PET,
+	ERROR_MSG_FETCH_USERNAME,
+	ERROR_MSG_FETCH_USER,
+	ERROR_MSG_UPDATE_USER,
+	ERROR_MSG_DELETE_PET,
+	ERROR_MSG_DELETE_PETS,
+	ERROR_MSG_CREATE_PET,
+	ERROR_MSG_DELETE_USER,
+	ERROR_MSG_USED_EMAIL,
+	ERROR_MSG_USED_PHONE,
+	ERROR_MSG_INCORRECT_PASSWORD,
+	ERROR_MSG_NOT_FOUND_USER,
+	SUCCESS_MSG_CREATED_USER,
+	SUCCESS_MSG_UPDATED_PET,
+	SUCCESS_MSG_UPDATED_USER,
+	SUCCESS_MSG_DELETED_PET,
+	SUCCESS_MSG_DELETED_PETS,
+	SUCCESS_MSG_DELETED_USER_AND_PETS,
+};
---
+++ types/queryTypes.js
@@ -0,0 +1,53 @@
+const SELECT_ALL = `SELECT * FROM`;
+const PAGINATION = `LIMIT $1 OFFSET $2`;
+const ORDER_BY_DESC_DATE = `ORDER BY since DESC`;
+const BY_ID = `WHERE id=$1`;
+const BY_USER_ID = `WHERE userId=$1`;
+const BY_EMAIL = `WHERE email=$1`;
+
+/* Pet Queries */
+const SELECT_ALL_PETS = `${SELECT_ALL} pets`;
+const SELECT_PETS_BY_DESC_DATE = `${SELECT_ALL_PETS} ${ORDER_BY_DESC_DATE}`;
+const SELECT_PETS_BY_PAGINATION = `${SELECT_PETS_BY_DESC_DATE} ${PAGINATION}`;
+const SELECT_TOTAL_NUM_OF_PETS = `SELECT COUNT(*) FROM pets`;
+const SELECT_PET_BY_ID = `${SELECT_ALL_PETS} ${BY_ID}`;
+const SELECT_PETS_BY_USER = `${SELECT_ALL_PETS} ${BY_USER_ID} ${ORDER_BY_DESC_DATE}`;
+
+const INSERT_PET_VALUES = `INSERT INTO pets(userId, img, petstatus, petlocation, longitude, latitude, species, petsize, breed, sex, color, age, uniquefeature, postdescription) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`;
+
+const UPDATE_PET = `UPDATE pets SET petstatus=$1, petlocation=$2, longitude=$3, latitude=$4, species=$5, petsize=$6, breed=$7, sex=$8, color=$9, age=$10, uniquefeature=$11, postdescription=$12 WHERE id=$13`;
+
+const DELETE_ALL_PETS = `DELETE FROM pets`;
+const DELETE_PET_BY_ID = `${DELETE_ALL_PETS} ${BY_ID}`;
+const DELETE_PET_BY_USER = `${DELETE_ALL_PETS} ${BY_USER_ID}`;
+
+/* User Queries */
+const SELECT_ALL_USERS = `${SELECT_ALL} users`;
+const SELECT_USER_BY_ID = `${SELECT_ALL_USERS} ${BY_ID}`;
+const SELECT_USER_BY_EMAIL = `${SELECT_ALL_USERS} ${BY_EMAIL}`;
+
+const INSERT_USER_VALUES = `INSERT INTO users(username, email, pw, phone) VALUES ($1, $2, $3, $4) RETURNING *`;
+
+const UPDATE_USER = `UPDATE users SET username=$1, email=$2, pw=$3, phone=$4 WHERE id=$5`;
+
+const DELETE_ALL_USERS = `DELETE FROM users`;
+const DELETE_USER_BY_ID = `${DELETE_ALL_USERS} ${BY_ID}`;
+
+export {
+	SELECT_PETS_BY_DESC_DATE,
+	SELECT_PETS_BY_PAGINATION,
+	SELECT_TOTAL_NUM_OF_PETS,
+	SELECT_PET_BY_ID,
+	SELECT_ALL_USERS,
+	SELECT_USER_BY_ID,
+	SELECT_PETS_BY_USER,
+	SELECT_USER_BY_EMAIL,
+	UPDATE_PET,
+	UPDATE_USER,
+	DELETE_ALL_PETS,
+	DELETE_PET_BY_ID,
+	DELETE_PET_BY_USER,
+	DELETE_USER_BY_ID,
+	INSERT_PET_VALUES,
+	INSERT_USER_VALUES,
+};
