95adf1bfa1fa2e8efffab3a6be1f5adde16498c3
Lilla Németh <naemly@protonmail.com>
2024-01-28 21:02:39 +0200

    fixing one error in the dashboard

--- frontend/src/components/generic/SelectAll.js
+++ frontend/src/components/generic/SelectAll.js
@@ -1,44 +1,36 @@
 import React from 'react';
 
-
 const SelectAll = (props) => {
-    const { 
-        deleteUserAllPets, 
-        allChecked, 
-        setAllChecked, 
-        petCardChecked, 
-        setPetCardChecked, 
-        disable 
-    } = props;
+	const { deleteUserAllPets, allChecked, setAllChecked, petCardChecked, setPetCardChecked, disable } = props;
 
-    let DEBUG = false;
+	let DEBUG = false;
 
-        return (
-            <div className='selectBox'>
-                <div className='deleteAllButtonBox'>
-                    <button 
-                        className={disable ? 'deleteAllButtonInactive' : 'deleteAllButton'}
-                        disable={disable}
-                        onClick={() => deleteUserAllPets()}
-                    >
-                        Delete All Pets
-                    </button>
-                </div>
-                <div>
-                    <label className='bigCheckboxContainer'>
-                      <input
-                        type='checkbox'
-                        checked={allChecked}
-                        onChange={() => {
-                            setAllChecked(!allChecked); 
-                            setPetCardChecked(petCardChecked)
-                        }}
-                      />
-                      <span className='bigCheckmark'></span>
-                    </label>
-                </div>
-            </div>
-        );
-}
+	return (
+		<div className='selectBox'>
+			<div className='deleteAllButtonBox'>
+				<button
+					className={disable === 'true' ? 'deleteAllButtonInactive' : 'deleteAllButton'}
+					disable={disable === 'true'}
+					onClick={() => deleteUserAllPets()}
+				>
+					Delete All Pets
+				</button>
+			</div>
+			<div>
+				<label className='bigCheckboxContainer'>
+					<input
+						type='checkbox'
+						checked={allChecked}
+						onChange={() => {
+							setAllChecked(!allChecked);
+							setPetCardChecked(petCardChecked);
+						}}
+					/>
+					<span className='bigCheckmark'></span>
+				</label>
+			</div>
+		</div>
+	);
+};
 
 export default SelectAll;
