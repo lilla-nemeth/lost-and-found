69f05d0ef35f693c26e412ffe57fda3f6debd526
Lilla Németh <naemly@protonmail.com>
2024-01-29 11:38:33 +0200

    ES6 version of migration files (user, pet); using references to specify userId as foreign key (pet)

--- sequelize/migrations/20240124191523-create-user.js
+++ sequelize/migrations/20240124191523-create-user.js
@@ -1,37 +1,35 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-	async up(queryInterface, Sequelize) {
-		await queryInterface.createTable('Users', {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER,
-			},
-			username: {
-				type: Sequelize.STRING,
-			},
-			email: {
-				type: Sequelize.STRING,
-			},
-			pw: {
-				type: Sequelize.STRING,
-			},
-			phone: {
-				type: Sequelize.STRING,
-			},
-			createdAt: {
-				allowNull: false,
-				type: Sequelize.DATE,
-			},
-			updatedAt: {
-				allowNull: false,
-				type: Sequelize.DATE,
-			},
-		});
-	},
-	async down(queryInterface, Sequelize) {
-		await queryInterface.dropTable('Users');
-	},
-};
+export async function up(queryInterface, Sequelize) {
+	await queryInterface.createTable('Users', {
+		id: {
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER,
+		},
+		username: {
+			type: Sequelize.STRING,
+		},
+		email: {
+			type: Sequelize.STRING,
+		},
+		pw: {
+			type: Sequelize.STRING,
+		},
+		phone: {
+			type: Sequelize.STRING,
+		},
+		createdAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+		updatedAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+	});
+}
+export async function down(queryInterface, Sequelize) {
+	await queryInterface.dropTable('Users');
+}
--- sequelize/migrations/20240124192320-create-pet.js
+++ sequelize/migrations/20240124192320-create-pet.js
@@ -1,70 +1,76 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-	async up(queryInterface, Sequelize) {
-		await queryInterface.createTable('Pets', {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER,
+export async function up(queryInterface, Sequelize) {
+	await queryInterface.createTable('Pets', {
+		id: {
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER,
+		},
+		userId: {
+			type: Sequelize.INTEGER,
+			references: {
+				model: {
+					tableName: 'users',
+					schema: 'schema',
+				},
+				key: 'id',
 			},
-			userId: {
-				type: Sequelize.INTEGER,
-			},
-			img: {
-				type: Sequelize.STRING,
-			},
-			petstatus: {
-				type: Sequelize.STRING,
-			},
-			longitude: {
-				type: Sequelize.STRING,
-			},
-			latitude: {
-				type: Sequelize.STRING,
-			},
-			petlocation: {
-				type: Sequelize.STRING,
-			},
-			species: {
-				type: Sequelize.STRING,
-			},
-			petsize: {
-				type: Sequelize.STRING,
-			},
-			breed: {
-				type: Sequelize.STRING,
-			},
-			sex: {
-				type: Sequelize.STRING,
-			},
-			color: {
-				type: Sequelize.STRING,
-			},
-			age: {
-				type: Sequelize.STRING,
-			},
-			uniquefeature: {
-				type: Sequelize.STRING,
-			},
-			postdescription: {
-				type: Sequelize.STRING,
-			},
-			since: {
-				type: Sequelize.DATE,
-			},
-			createdAt: {
-				allowNull: false,
-				type: Sequelize.DATE,
-			},
-			updatedAt: {
-				allowNull: false,
-				type: Sequelize.DATE,
-			},
-		});
-	},
-	async down(queryInterface, Sequelize) {
-		await queryInterface.dropTable('Pets');
-	},
-};
+			allowNull: false,
+		},
+		img: {
+			type: Sequelize.STRING,
+		},
+		petstatus: {
+			type: Sequelize.STRING,
+		},
+		longitude: {
+			type: Sequelize.STRING,
+		},
+		latitude: {
+			type: Sequelize.STRING,
+		},
+		petlocation: {
+			type: Sequelize.STRING,
+		},
+		species: {
+			type: Sequelize.STRING,
+		},
+		petsize: {
+			type: Sequelize.STRING,
+		},
+		breed: {
+			type: Sequelize.STRING,
+		},
+		sex: {
+			type: Sequelize.STRING,
+		},
+		color: {
+			type: Sequelize.STRING,
+		},
+		age: {
+			type: Sequelize.STRING,
+		},
+		uniquefeature: {
+			type: Sequelize.STRING,
+		},
+		postdescription: {
+			type: Sequelize.STRING,
+		},
+		since: {
+			type: Sequelize.DATE,
+		},
+		createdAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+		updatedAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+	});
+}
+export async function down(queryInterface, Sequelize) {
+	await queryInterface.dropTable('Pets');
+}
