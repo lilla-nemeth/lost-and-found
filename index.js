3cf9b4bdbb7aae24628c5453299f5b3993b997dc
Lilla Németh <naemly@protonmail.com>
2024-01-29 11:56:55 +0200

    sequelize connection pool configuration for reusable connection to DB

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ const port = process.env.PORT || 3003;
 const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
 	host: process.env.PG_HOST,
 	dialect: 'postgres',
+	pool: {
+		max: 9,
+		min: 0,
+		acquire: 30000,
+		idle: 10000,
+	},
 });
 
 sequelize
