b8970068eb68d855051d62bbb3c4f5f28595b547
Lilla Németh <naemly@protonmail.com>
2024-02-21 00:01:01 +0200

    test for getAll

--- server/src/controllers/clientControllers.ts
+++ server/src/controllers/clientControllers.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import path from 'path';
 
 const getAll = async (request: Request, response: Response): Promise<void> => {
-	response.sendFile(path.join(__dirname, 'client/build/index.html'));
+	response.sendFile(path.join(__dirname, '../client/build/index.html'));
 };
 
 export { getAll };
--- server/src/tests/controllers/clientControllers.test.ts
+++ server/src/tests/controllers/clientControllers.test.ts
@@ -1,4 +1,19 @@
 import { getAll } from '../../controllers/clientControllers';
 import { Request, Response } from 'express';
+import path from 'path';
 
 // getAll
+describe('getAll', () => {
+	it('should send the HTML file', async () => {
+		const mReq = {} as Request;
+		const mRes = {
+			sendFile: jest.fn(),
+		} as unknown as Response;
+
+		await getAll(mReq, mRes);
+
+		const expectedFilePath = path.join(__dirname, '../../client/build/index.html');
+
+		expect(mRes.sendFile).toHaveBeenCalledWith(expectedFilePath);
+	});
+});
